0342f5895a918f67107b91c40f43885b8d6887e2
ﾆｬｧｧｧ-╬╬(╬^ಠｘಠ)♭ <yokomichi@it-nasu.com>
2018-09-03 10:35:00 +0900

    まとめてコミット2

--- bookshelf_api/routes/index.js
+++ bookshelf_api/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var connection = require('../mysqlconnection');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var query = 'SELECT *, DATE_FORMAT(created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM authors';
+  connection.query(query, function(err, rows) {
+    console.log(rows);
+    res.render('index', { title: 'Express' });
+  });
 });
 
 module.exports = router;
