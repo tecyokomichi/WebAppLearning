7565ab6f5708a9db0aebb54823569e1bae67d817
ﾆｬｧｧｧ-╬╬(╬^ಠｘಠ)♭ <yokomichi@it-nasu.com>
2019-07-25 18:05:44 +0900

    add moretypes example

---
+++ go/moretypes/slice-len-cap/slice-len-cap.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+    s := []int{2, 3, 5, 7, 11, 13}
+    fmt.Println(s)
+
+    // Slice the slice to give it zero length.
+    s = s[:0]
+    printSlice(s)
+
+    // Extend its length.
+    s = s[:4] 
+    printSlice(s)
+
+    // Drop its first two values.
+    fmt.Printf("cap=%d\n", cap(s))
+    s = s[2:]
+    printSlice(s)
+}
+
+func printSlice(s []int) { 
+    fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
+
