0559291270070b0a668099489afc5e3c8ca385ac
ﾆｬｧｧｧ-╬╬(╬^ಠｘಠ)♭ <yokomichi@it-nasu.com>
2020-07-28 15:50:46 +0900

    Hello Sagas!

--- typescript/redux-saga/redux-saga-beginner-tutorial/main.js
+++ typescript/redux-saga/redux-saga-beginner-tutorial/main.js
@@ -3,11 +3,19 @@ import "babel-polyfill"
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 
 import Counter from './Counter'
 import reducer from './reducers'
 
-const store = createStore(reducer)
+import { helloSaga } from './sagas'
+
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware)
+)
+sagaMiddleware.run(helloSaga)
 
 const action = type => store.dispatch({type})
 
