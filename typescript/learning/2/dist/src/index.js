584f502b98bf9dc51ac20ffed97d0b3058c8c209
ﾆｬｧｧｧ-╬╬(╬^ಠｘಠ)♭ <yokomichi@it-nasu.com>
2021-01-12 08:29:48 +0900

    add lesson2

---
+++ typescript/learning/2/dist/src/index.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log('Hello TypeScript');
+let a = 1 + 2;
+let b = a + 3;
+let c = {
+    apple: a,
+    banana: b
+};
+let d = c.apple * 4;
+console.log(d);
+//# sourceMappingURL=index.js.map
