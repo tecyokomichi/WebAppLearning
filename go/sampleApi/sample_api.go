099b74100dbbde3a6632b4ba2840032b527343b9
ﾆｬｧｧｧ-╬╬(╬^ಠｘಠ)♭ <yokomichi@it-nasu.com>
2022-02-24 10:01:04 +0900

    show を修正
    指定された id で user を返す

--- go/sampleApi/sample_api.go
+++ go/sampleApi/sample_api.go
@@ -21,7 +21,15 @@ func NewSampleAPI(logger *log.Logger, db *sql.DB) sampleapi.Service {
 
 // Show implements show.
 func (s *sampleAPIsrvc) Show(ctx context.Context, p *sampleapi.ShowPayload) (res *sampleapi.User, err error) {
-	res = &sampleapi.User{}
 	s.logger.Print("sampleAPI.show")
+	var id int
+	var name, email string
+	err = s.DB.QueryRow("SELECT id, name, email FROM users WHERE id=?", p.ID).Scan(&id, &name, &email)
+	if err != nil {
+		s.logger.Print(err)
+		return
+	}
+	s.logger.Print("id: ", id, " | name: ", name, " | email: ", email)
+	res = &sampleapi.User{ID: &id, Name: &name, Email: &email}
 	return
 }
