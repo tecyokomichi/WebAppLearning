41990271df26dfd09c3f0c8c2dfa3e4e841c983e
ﾆｬｧｧｧ-╬╬(╬^ಠｘಠ)♭ <yokomichi@it-nasu.com>
2022-09-27 14:02:06 +0900

    :+1: 足し算アプリ

--- go/study/ch02_first_cli_app/main.go
+++ go/study/ch02_first_cli_app/main.go
@@ -3,12 +3,28 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
-	var name = "someone"
-	if len(os.Args) > 1 {
-		name = os.Args[1]
+	if len(os.Args) < 3 || 3 < len(os.Args) {
+		fmt.Println("USAGE:")
+		fmt.Println("./ch02_first_cli X Y")
+		fmt.Println(" Shows sum of X and Y")
+		fmt.Println(" X and Y must be number")
+		os.Exit(1)
+	} else if len(os.Args) == 3 {
+		x, e := strconv.Atoi(os.Args[1])
+		if  e != nil {
+			fmt.Printf("ERROR: %q is not a number\n", os.Args[1])
+			os.Exit(1)
+		}
+		y, e := strconv.Atoi(os.Args[2])
+		if  e != nil {
+			fmt.Printf("ERROR: %q is not a number\n", os.Args[2])
+			os.Exit(1)
+		}
+		fmt.Printf("Rrresult: %d\n", x+y)
+		os.Exit(0)
 	}
-	fmt.Printf("Hello %s!\n", name)
 }
