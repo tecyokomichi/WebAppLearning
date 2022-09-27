0eeef31e0b31270a8041871258109fb14af25fbd
ﾆｬｧｧｧ-╬╬(╬^ಠｘಠ)♭ <yokomichi@it-nasu.com>
2022-09-27 11:05:03 +0900

    :green_heart: Subtract のテストを追加

--- go/study/second_tour_of_go-ch01_first_test_01/ch01_first_test/main_test.go
+++ go/study/second_tour_of_go-ch01_first_test_01/ch01_first_test/main_test.go
@@ -10,3 +10,14 @@ func TestAdd(t *testing.T) {
 		t.FailNow()
 	}
 }
+
+func TestSubtract(t *testing.T) {
+	s := Subtract(1, 2)
+	if s != -1 {
+		t.FailNow()
+	}
+	s = Subtract(2, 1)
+	if s != 1 {
+		t.FailNow()
+	}
+}
