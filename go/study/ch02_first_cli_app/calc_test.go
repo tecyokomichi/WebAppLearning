976708264234b2345bf14b527093a1cd8e4d620f
ﾆｬｧｧｧ-╬╬(╬^ಠｘಠ)♭ <yokomichi@it-nasu.com>
2022-09-27 15:52:11 +0900

    :green_heart: calc_test.go を追加

---
+++ go/study/ch02_first_cli_app/calc_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestAddtion(t *testing.T) {
+	calc := &Addition{}
+	if calc.Kind() != "addition" {
+		t.FailNow()
+	}
+	if calc.Do(1, 2) != 3 {
+		t.FailNow()
+	}
+}
+
+func TestSubtraction(t *testing.T) {
+	calc := &Subtraction{}
+	if calc.Kind() != "subtraction" {
+		t.FailNow()
+	}
+	if calc.Do(2, 1) != 1 {
+		t.FailNow()
+	}
+}
