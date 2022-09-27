248546032427d3be8f2590ec9e8db1a313b74c64
ﾆｬｧｧｧ-╬╬(╬^ಠｘಠ)♭ <yokomichi@it-nasu.com>
2022-09-27 13:27:46 +0900

    :+1: Hello, Arguments

--- go/study/ch02_first_cli_app/main.go
+++ go/study/ch02_first_cli_app/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	println("Hello, World!")
+	var name = "someone"
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	fmt.Printf("Hello %s!\n", name)
 }
