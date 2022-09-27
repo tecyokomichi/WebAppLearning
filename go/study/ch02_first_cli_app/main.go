432d5f71696b345fd45bbdbeff0f0fe331f8b59a
ﾆｬｧｧｧ-╬╬(╬^ಠｘಠ)♭ <yokomichi@it-nasu.com>
2022-09-27 14:27:42 +0900

    :+1: 足し算と引き算のサポート

--- go/study/ch02_first_cli_app/main.go
+++ go/study/ch02_first_cli_app/main.go
@@ -7,24 +7,38 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 3 || 3 < len(os.Args) {
-		fmt.Println("USAGE:")
-		fmt.Println("./ch02_first_cli X Y")
-		fmt.Println(" Shows sum of X and Y")
-		fmt.Println(" X and Y must be number")
+	if len(os.Args) < 4 || 4 < len(os.Args) {
+		showHelp()
 		os.Exit(1)
-	} else if len(os.Args) == 3 {
-		x, e := strconv.Atoi(os.Args[1])
-		if  e != nil {
-			fmt.Printf("ERROR: %q is not a number\n", os.Args[1])
+	} else if len(os.Args) == 4 {
+		var kind string
+		switch os.Args[1] {
+		case "add":
+			kind = "addition"
+		case "subtract":
+			kind = "subtraction"
+		default:
+			showHelp()
 			os.Exit(1)
 		}
-		y, e := strconv.Atoi(os.Args[2])
+		x, e := strconv.Atoi(os.Args[2])
 		if  e != nil {
 			fmt.Printf("ERROR: %q is not a number\n", os.Args[2])
 			os.Exit(1)
 		}
-		fmt.Printf("Rrresult: %d\n", x+y)
+		y, e := strconv.Atoi(os.Args[3])
+		if  e != nil {
+			fmt.Printf("ERROR: %q is not a number\n", os.Args[3])
+			os.Exit(1)
+		}
+		var r int
+		switch kind {
+		case "addition":
+			r = x + y
+		case "subtraction":
+			r = x - y
+		}
+		fmt.Printf("%s: %d\n", kind, r)
 		os.Exit(0)
 	}
 }
