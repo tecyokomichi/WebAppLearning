866aa4e44823bda38cc7216b2c645fa924d3b7fe
ﾆｬｧｧｧ-╬╬(╬^ಠｘಠ)♭ <yokomichi@it-nasu.com>
2022-10-07 14:58:12 +0900

    :green_heart: テストの修正と追加

--- go/study/ch02_first_cli_app/calc_test.go
+++ go/study/ch02_first_cli_app/calc_test.go
@@ -7,7 +7,7 @@ func TestAddtion(t *testing.T) {
 	if calc.Kind() != "addition" {
 		t.FailNow()
 	}
-	if calc.Do(1, 2) != 3 {
+	if calc.Do(1, 2) != Int(3) {
 		t.FailNow()
 	}
 }
@@ -17,7 +17,7 @@ func TestSubtraction(t *testing.T) {
 	if calc.Kind() != "subtraction" {
 		t.FailNow()
 	}
-	if calc.Do(2, 1) != 1 {
+	if calc.Do(2, 1) != Int(1) {
 		t.FailNow()
 	}
 }
@@ -27,7 +27,22 @@ func TestMultiplication(t *testing.T) {
 	if calc.Kind() != "multiplication" {
 		t.FailNow()
 	}
-	if calc.Do(2, 3) != 6 {
+	if calc.Do(2, 3) != Int(6) {
 		t.FailNow()
+	}
+}
+
+func TestDivision(t *testing.T) {
+	calc := &Division{}
+	if calc.Kind() != "division" {
+		t.FailNow()
+	}
+	if _, ok := calc.Do(2, 0).(*InvalidDenominator); !ok {
+		t.FailNow()
+	}
+	if r, ok := calc.Do(10, 3).(*DivisionResult); ok {
+		if r.Quotient != 3 || r.Remainder != 1 {
+			t.FailNow()
+		}
 	}
 }
