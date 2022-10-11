c076bca726ebaa44dda2a9df3def82db55c48743
ﾆｬｧｧｧ-╬╬(╬^ಠｘಠ)♭ <yokomichi@it-nasu.com>
2022-10-11 13:30:58 +0900

    :green_heart: ExpandCircle のテストを追加

--- go/study/base/main_test.go
+++ go/study/base/main_test.go
@@ -9,3 +9,12 @@ func TestNewCircle(t *testing.T) {
 		t.FailNow()
 	}
 }
+
+func TestExpandCircle(t *testing.T) {
+	p := &Point{Px: 100, Py: 200}
+	c := NewCircle(p, 300)
+	ExpandCircle(c, 400)
+	if c.radius != 700 {
+		t.FailNow()
+	}
+}
