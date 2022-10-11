2a3c3f5d92702f246ecf78de588bf82717393beb
ﾆｬｧｧｧ-╬╬(╬^ಠｘಠ)♭ <yokomichi@it-nasu.com>
2022-10-11 17:00:12 +0900

    :+1: Biggest を実装

--- go/study/base/main.go
+++ go/study/base/main.go
@@ -12,5 +12,10 @@ func main() {
 	fmt.Printf("%#v\n", c1)
 
 	r1 := &Rect{Point: p1, width: 20, length: 10}
+	r2 := &Rect{Point: p1, width: 30, length: 20}
+	r3 := &Rect{Point: p1, width: 40, length: 30}
+	r4 := &Rect{Point: p1, width: 50, length: 40}
+	r5 := &Rect{Point: p1, width: 10, length: 5}
 	fmt.Printf("%#v\n", r1)
+	fmt.Printf("%#v\n", RecList([]*Rect{r1, r2, r3, r4, r5}).Biggest())
 }
