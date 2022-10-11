1077293b43ab4dfc7a8232c20d90da45c077b3c2
ﾆｬｧｧｧ-╬╬(╬^ಠｘಠ)♭ <yokomichi@it-nasu.com>
2022-10-11 11:30:26 +0900

    :+1: 出力

--- go/study/base/main.go
+++ go/study/base/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Point struct {
 	Px int
 	Py int
@@ -14,4 +16,22 @@ type Rect struct {
 	width int
 	length int
 	Point *Point
+}
+
+func main() {
+	p1 := new(Point)
+	p1.Px = 100
+	p1.Py = 50
+	fmt.Printf("%#v\n", p1)
+
+	c1 := new(Circle)
+	c1.Point = p1
+	c1.radius = 30
+	fmt.Printf("%#v\n", c1)
+
+	r1 := new(Rect)
+	r1.Point = p1
+	r1.width = 20
+	r1.length = 10
+	fmt.Printf("%#v\n", r1)
 }
