7721b1e9d7832d75564671238c3f9f28b3ea714c
ﾆｬｧｧｧ-╬╬(╬^ಠｘಠ)♭ <yokomichi@it-nasu.com>
2022-10-12 13:49:47 +0900

    green_heart Shapes Biggest テストを追加

--- go/study/base/main_test.go
+++ go/study/base/main_test.go
@@ -29,10 +29,14 @@ func TestArea(t *testing.T) {
 
 func TestBiggest(t *testing.T) {
 	p := &Point{Px: 100, Py: 200}
+	c := NewCircle(p, 300)
 	r1 := &Rect{Point: p, width: 300, length: 400}
 	r2 := &Rect{Point: p, width: 30, length: 40}
 	r3 := &Rect{Point: p, width: 3, length: 4}
 	if RecList([]*Rect{r1, r2, r3}).Biggest().Area() != 120000 {
+		t.FailNow()
+	}
+	if Shapes([]Shape{c, r1, r2, r3}).Biggest().Area() < 282743 || Shapes([]Shape{c, r1, r2, r3}).Biggest().Area() > 282744 {
 		t.FailNow()
 	}
 }
