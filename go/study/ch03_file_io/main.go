9aeb113865abf38cd979c2d9c338785c7de39c50
ﾆｬｧｧｧ-╬╬(╬^ಠｘಠ)♭ <yokomichi@it-nasu.com>
2022-11-16 08:53:30 +0900

    :+1: サブコマンド write を実装

--- go/study/ch03_file_io/main.go
+++ go/study/ch03_file_io/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -19,6 +20,33 @@ func main() {
 			os.Exit(1)
 		}
 		fmt.Println(string(f))
+	case "write":
+		if len (os.Args) != 4 {
+			showHelp()
+			os.Exit(1)
+		}
+		_, e := os.Stat(os.Args[2])
+		if e == nil {
+			fmt.Printf("%s already exists. Overwrite? (y/n): ", os.Args[2])
+			var res string
+			fmt.Scanln(&res)
+			if !strings.HasPrefix(strings.ToLower(res), "y") {
+				fmt.Println("Quit writing")
+				os.Exit(1)
+			}
+		}
+		f, err := os.Create(os.Args[2])
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		defer f.Close()
+		_, ee := fmt.Fprintln(f, os.Args[3])
+		if ee != nil{
+			fmt.Fprintln(os.Stderr, ee)
+			os.Exit(1)
+		}
+
 	default:
 		showHelp()
 		os.Exit(1)
