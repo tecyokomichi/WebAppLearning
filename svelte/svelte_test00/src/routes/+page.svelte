0929a0681083f1493c504d1073b09b025ae0d7d3
ﾆｬｧｧｧ-╬╬(╬^ಠｘಠ)♭ <yokomichi@it-nasu.com>
2023-10-31 11:04:20 +0900

    :+1: Andding data

--- svelte/svelte_test00/src/routes/+page.svelte
+++ svelte/svelte_test00/src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+  let name = 'svelte';
+</script>
+
+<h1>Welcome to { name.toUpperCase() }</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
