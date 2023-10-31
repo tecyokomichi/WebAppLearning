6be77cfd567a2e814469e781f2b5d623ce81a293
ﾆｬｧｧｧ-╬╬(╬^ಠｘಠ)♭ <yokomichi@it-nasu.com>
2023-10-31 11:38:42 +0900

    :+1: Assignments

--- svelte/svelte_test00/src/routes/+page.svelte
+++ svelte/svelte_test00/src/routes/+page.svelte
@@ -3,11 +3,21 @@
 
   let name = 'svelte';
   let src = 'images/svelte.png';
+  let count = 0;
+
+  function increment() {
+    count += 1;
+  }
 </script>
 
 <h1>Welcome to { name.toUpperCase() }</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <Nested />
+
+<button on:click={ increment }>
+  Clicked { count }
+  { count === 1 ? 'time' : 'times' }
+</button>
 
 
 <img { src } alt="{ name } dances." />
