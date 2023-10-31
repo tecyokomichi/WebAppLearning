999813a46b2ee579355ffdfbbf978c2ed3b3c364
ﾆｬｧｧｧ-╬╬(╬^ಠｘಠ)♭ <yokomichi@it-nasu.com>
2023-10-31 15:22:04 +0900

    :+1: Keyed each blocks

--- svelte/svelte_test00/src/App.svelte
+++ svelte/svelte_test00/src/App.svelte
@@ -1,6 +1,18 @@
 <script>
+  import Thing from './Thing.svelte';
 	const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 	let selected = colors[0];
+  let things = [
+		{ id: 1, name: 'apple' },
+		{ id: 2, name: 'banana' },
+		{ id: 3, name: 'carrot' },
+		{ id: 4, name: 'doughnut' },
+		{ id: 5, name: 'egg' }
+	];
+
+	function handleClick() {
+		things = things.slice(1);
+	}
 </script>
 
 <h1 style="color: {selected}">Pick a colour</h1>
@@ -8,13 +20,21 @@
 <div>
 	{#each colors as color, i}
     <button
-      aria-current={selected === color}
+      aria-current={ selected === color }
       aria-label={ color }
       style="background: { color };"
-      on:click={() => selected = color}
+      on:click={ () => selected = color }
     >{ i + 1 }</button>
   {/each}
 </div>
+
+<button on:click={ handleClick }>
+	Remove first thing
+</button>
+
+{#each things as thing (thing.id)}
+	<Thing name={ thing.name } />
+{/each}
 
 <style>
 	h1 {
