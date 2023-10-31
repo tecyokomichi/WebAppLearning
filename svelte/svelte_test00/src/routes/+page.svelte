15d92fae95ebc48bd23fdabbe292aa7d7b10d3dd
ﾆｬｧｧｧ-╬╬(╬^ಠｘಠ)♭ <yokomichi@it-nasu.com>
2023-10-31 11:10:58 +0900

    :+1: Dynamic and Shorthand attributes

--- svelte/svelte_test00/src/routes/+page.svelte
+++ svelte/svelte_test00/src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script>
   let name = 'svelte';
+  let src = 'images/svelte.png';
 </script>
 
 <h1>Welcome to { name.toUpperCase() }</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<img { src } alt="{ name } dances." />
