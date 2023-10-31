b2db8c613685189cb5991306cf25c89c41d9beae
ﾆｬｧｧｧ-╬╬(╬^ಠｘಠ)♭ <yokomichi@it-nasu.com>
2023-10-31 11:24:11 +0900

    :+1: Nested components HTML要素と区別するためコンポーネント名称は常に大文字で始まる

--- svelte/svelte_test00/src/routes/+page.svelte
+++ svelte/svelte_test00/src/routes/+page.svelte
@@ -1,10 +1,14 @@
 <script>
+  import Nested from '../Nested.svelte';
+
   let name = 'svelte';
   let src = 'images/svelte.png';
 </script>
 
 <h1>Welcome to { name.toUpperCase() }</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Nested />
+
 
 <img { src } alt="{ name } dances." />
 
