478dc133606c36def4d178b8f0f3b3b5d744c439
ﾆｬｧｧｧ-╬╬(╬^ಠｘಠ)♭ <yokomichi@it-nasu.com>
2023-12-08 16:20:44 +0900

    :+1: 罫線表示

--- svelte/sveltekit-app/src/routes/car/+page.svelte
+++ svelte/sveltekit-app/src/routes/car/+page.svelte
@@ -13,31 +13,31 @@
 
 <Table class="w-full">
     <TableHead>
-      <TableHeadCell >コード</TableHeadCell>
-      <TableHeadCell >車両登録番号</TableHeadCell>
-      <TableHeadCell >得意先名</TableHeadCell>
-      <TableHeadCell >登録日</TableHeadCell>
-      <TableHeadCell >初度</TableHeadCell>
-      <TableHeadCell >車検日</TableHeadCell>
-      <TableHeadCell >前入庫日</TableHeadCell>
-      <TableHeadCell >車名</TableHeadCell>
-      <TableHeadCell >通称名</TableHeadCell>
-      <TableHeadCell >型式</TableHeadCell>
-      <TableHeadCell ></TableHeadCell>
+      <TableHeadCell class="border border-2 border-green-500 px-6 py-3" >コード</TableHeadCell>
+      <TableHeadCell class="border border-2 border-green-500 px-6 py-3" >車両登録番号</TableHeadCell>
+      <TableHeadCell class="border border-2 border-green-500 px-6 py-3" >得意先名</TableHeadCell>
+      <TableHeadCell class="border border-2 border-green-500 px-6 py-3" >登録日</TableHeadCell>
+      <TableHeadCell class="border border-2 border-green-500 px-6 py-3" >初度</TableHeadCell>
+      <TableHeadCell class="border border-2 border-green-500 px-6 py-3" >車検日</TableHeadCell>
+      <TableHeadCell class="border border-2 border-green-500 px-6 py-3" >前入庫日</TableHeadCell>
+      <TableHeadCell class="border border-2 border-green-500 px-6 py-3" >車名</TableHeadCell>
+      <TableHeadCell class="border border-2 border-green-500 px-6 py-3" >通称名</TableHeadCell>
+      <TableHeadCell class="border border-2 border-green-500 px-6 py-3" >型式</TableHeadCell>
+      <TableHeadCell class="border border-2 border-green-500 px-6 py-3" ></TableHeadCell>
     </TableHead>
     <TableBody>
         {#each car as item, i (item.id)}
             <tr>
-                <TableBodyCell>{item.code}</TableBodyCell>
-                <TableBodyCell>{item.registword}</TableBodyCell>
-                <TableBodyCell>{item.customerName}</TableBodyCell>
-                <TableBodyCell>{item.regsitDate}</TableBodyCell>
-                <TableBodyCell>{item.regsitFirstDate}</TableBodyCell>
-                <TableBodyCell>{item.inspectionDate}</TableBodyCell>
-                <TableBodyCell>{item.storingDate}</TableBodyCell>
-                <TableBodyCell>{item.carMaker}</TableBodyCell>
-                <TableBodyCell>{item.carName}</TableBodyCell>
-                <TableBodyCell>{item.carModel}</TableBodyCell>
+                <TableBodyCell class="border border-2 border-green-500 px-6 py-3">{item.code}</TableBodyCell>
+                <TableBodyCell class="border border-2 border-green-500 px-6 py-3">{item.registword}</TableBodyCell>
+                <TableBodyCell class="border border-2 border-green-500 px-6 py-3">{item.customerName}</TableBodyCell>
+                <TableBodyCell class="border border-2 border-green-500 px-6 py-3">{item.regsitDate}</TableBodyCell>
+                <TableBodyCell class="border border-2 border-green-500 px-6 py-3">{item.regsitFirstDate}</TableBodyCell>
+                <TableBodyCell class="border border-2 border-green-500 px-6 py-3">{item.inspectionDate}</TableBodyCell>
+                <TableBodyCell class="border border-2 border-green-500 px-6 py-3">{item.storingDate}</TableBodyCell>
+                <TableBodyCell class="border border-2 border-green-500 px-6 py-3">{item.carMaker}</TableBodyCell>
+                <TableBodyCell class="border border-2 border-green-500 px-6 py-3">{item.carName}</TableBodyCell>
+                <TableBodyCell class="border border-2 border-green-500 px-6 py-3">{item.carModel}</TableBodyCell>
             </tr>
         {/each}
     </TableBody>
